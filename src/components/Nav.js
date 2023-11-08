2f1486fdefe9461e615f07b042d7a4af89f7f76b
yurimeeee <kplhn6@gmail.com>
2023-11-08 22:48:21 +0900

    [FIX] 모바일 버전 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,84 +1,203 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import { Plus, X } from "react-bootstrap-icons";
 
 const Nav = () => {
-  // let menuItems = document.querySelectorAll(".menu > ul > li");
-  let header = document.querySelector("header");
-  const [isHovered, setIsHovered] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activeSubMenu, setActiveSubMenu] = useState(null);
+  const mobileMenuRef = useRef(null);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
+  //브라우저 너비 확인
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 501);
+    };
+    //초기화
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
+  //메뉴 여닫기
+  useEffect(() => {
+    if (mobileMenuRef.current) {
+      mobileMenuRef.current.style.left = isMenuOpen
+        ? "calc(-100vw + 52px)"
+        : "100%";
+    }
+  }, [isMenuOpen]);
+
+  //서브메뉴 토글
+  const toggleSubMenu = (menu) => {
+    setActiveSubMenu(activeSubMenu === menu ? null : menu);
   };
-
-  // menuItems.forEach((item) => {
-  //   item.addEventListener("mouseover", () => {
-  //     header.style.height = "120px";
-  //     header.style.background = "rgba(14, 18, 42, 0.8)";
-  //   });
-  //   item.addEventListener("mouseout", () => {
-  //     header.style.height = "0";
-  //     header.style.background = "transparent"; // 또는 원하는 배경 스타일로 변경
-  //   });
-  // });
-
   return (
-    <nav className="menu">
-      <ul>
-        {/* <li
-          className={`${isHovered ? "active" : ""}`}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        > */}
-        <li>
-          <Link to="/Preview">관람 참여</Link>
-          <ul className="submenu">
-            <li>서울</li>
-            <li>과천</li>
-            <li>덕수궁</li>
-            <li>청주</li>
-            <li>어린이 미술관</li>
+    <div>
+      {isMobile ? (
+        //모바일 메뉴 구성
+        <div className="menu-wrap">
+          <button onClick={toggleMenu} className="m-menu-btn">
+            <Plus size={40} />
+          </button>
+          <div className="m-menu-bg container" ref={mobileMenuRef}>
+            <div className="m-btn-wrap">
+              <button onClick={toggleMenu} className="menu-close">
+                <X size={40} />
+              </button>
+            </div>
+            <nav className="m-menu">
+              <ul>
+                <li onClick={() => toggleSubMenu("관람 참여")}>
+                  관람 참여
+                  <ul
+                    className={
+                      activeSubMenu === "관람 참여"
+                        ? "m-submenu active"
+                        : "m-submenu"
+                    }
+                  >
+                    <li>서울</li>
+                    <li>과천</li>
+                    <li>덕수궁</li>
+                    <li>청주</li>
+                    <li>어린이 미술관</li>
+                  </ul>
+                </li>
+                <li onClick={() => toggleSubMenu("전시")}>
+                  전시
+                  <ul
+                    className={
+                      activeSubMenu === "전시"
+                        ? "m-submenu active"
+                        : "m-submenu"
+                    }
+                  >
+                    <li>현재 전시</li>
+                    <li>예정 전시</li>
+                    <li>과거 전시</li>
+                  </ul>
+                </li>
+                <li onClick={() => toggleSubMenu("교육")}>
+                  교육
+                  <ul
+                    className={
+                      activeSubMenu === "교육"
+                        ? "m-submenu active"
+                        : "m-submenu"
+                    }
+                  >
+                    <li>미술관 교육소개</li>
+                  </ul>
+                </li>
+                <li onClick={() => toggleSubMenu("소장품")}>
+                  소장품
+                  <ul
+                    className={
+                      activeSubMenu === "소장품"
+                        ? "m-submenu active"
+                        : "m-submenu"
+                    }
+                  >
+                    <li>소장품 검색</li>
+                    <li>소장품 탐색</li>
+                    <li>보존 과학</li>
+                  </ul>
+                </li>
+                <li onClick={() => toggleSubMenu("미술연구")}>
+                  미술연구
+                  <ul
+                    className={
+                      activeSubMenu === "미술연구"
+                        ? "m-submenu active"
+                        : "m-submenu"
+                    }
+                  >
+                    <li>도서와 아카이브</li>
+                    <li>연구</li>
+                    <li>발간 자료</li>
+                  </ul>
+                </li>
+                <li onClick={() => toggleSubMenu("디지털 미술관")}>
+                  디지털 미술관
+                  <ul
+                    className={
+                      activeSubMenu === "디지털 미술관"
+                        ? "m-submenu active"
+                        : "m-submenu"
+                    }
+                  >
+                    <li>맞춤 검색</li>
+                    <li>오디오 가이드</li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          {/* )} */}
+        </div>
+      ) : (
+        //pc, 테블릿 메뉴 구성
+        <nav className="menu">
+          <ul>
+            <li>
+              <Link to="/Preview">관람 참여</Link>
+              <ul className="submenu">
+                <li>서울</li>
+                <li>과천</li>
+                <li>덕수궁</li>
+                <li>청주</li>
+                <li>어린이 미술관</li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/exhibit">전시</Link>
+              <ul className="submenu">
+                <li>현재 전시</li>
+                <li>예정 전시</li>
+                <li>과거 전시</li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/education">교육</Link>
+              <ul className="submenu">
+                <li>미술관 교육소개</li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/collection">소장품</Link>
+              <ul className="submenu">
+                <li>소장품 검색</li>
+                <li>소장품 탐색</li>
+                <li>보존 과학</li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/research">미술연구</Link>
+              <ul className="submenu">
+                <li>도서와 아카이브</li>
+                <li>연구</li>
+                <li>발간 자료</li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/digital">디지털 미술관</Link>
+              <ul className="submenu">
+                <li>맞춤 검색</li>
+                <li>오디오 가이드</li>
+              </ul>
+            </li>
           </ul>
-        </li>
-        <li>
-          <Link to="/exhibit">전시</Link>
-          <ul className="submenu">
-            <li>현재 전시</li>
-            <li>예정 전시</li>
-            <li>과거 전시</li>
-          </ul>
-        </li>
-        <li>
-          <Link to="/education">교육</Link>
-        </li>
-        <li>
-          <Link to="/collection">소장품</Link>
-          <ul className="submenu">
-            <li>소장품 검색</li>
-            <li>소장품 탐색</li>
-            <li>보존 과학</li>
-          </ul>
-        </li>
-        <li>
-          <Link to="/research">미술연구</Link>
-          <ul className="submenu">
-            <li>도서와 아카이브</li>
-            <li>연구</li>
-            <li>발간 자료</li>
-          </ul>
-        </li>
-        <li>
-          <Link to="/digital">디지털 미술관</Link>
-          <ul className="submenu">
-            <li>맞춤 검색</li>
-            <li>오디오 가이드</li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
+        </nav>
+      )}
+    </div>
   );
 };
 
